1b93221d3e18334d95e7bb4e5469537abcc1371c
notalphacoder113 <sigma.zulkar.2022@gmail.com>
2026-04-14 06:57:53 +0600

    use data from backend to generate the HTML.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,23 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products';
 import './HomePage.css'
 
 
 export function HomePage() {
+    // update the data 
+    const [products, setProducts] = useState([]);
+
+
     // fetch the data from backend instead of feact('url');
-    axios.get('http://localhost:3000/api/products')
-    .then((response)=>{
-        console.log(response.data);
-    })
-    
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+                setProducts(response.data); // updater function of useState()
+            })
+    }, []); // dependency array = helps us to control useEffect() ,here runs only once
+
+
 
     return (
         <>
