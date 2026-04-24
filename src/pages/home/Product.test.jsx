1187e08914845cc7b531cf6c467121058796737b
notalphacoder113 <sigma.zulkar.2022@gmail.com>
2026-04-24 10:02:22 +0600

    Use beforeEach to remove duplication.

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
@@ -6,10 +6,12 @@ import { Product } from "./Product";
 
 vi.mock("axios");
 
-
 describe("Product component", () => {
-    it("display the product details correctly", () => {
-        const product = {
+    let product; 
+    let loadCart;
+
+    beforeEach(()=>{
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,10 +22,13 @@ describe("Product component", () => {
             priceCents: 1090,
             keywords: ["socks", "sports", "apparel"]
         };
+        loadCart = vi.fn();
+    });
 
-        const loadCart = vi.fn();
-        render(<Product product={product} loadCart={loadCart}/>);
-        
+    it("display the product details correctly", () => {
+
+        render(<Product product={product} loadCart={loadCart} />);
+
         expect(
             screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs")
         ).toBeInTheDocument();
@@ -31,7 +36,7 @@ describe("Product component", () => {
         expect(
             screen.getByText("$10.90")
         ).toBeInTheDocument();
-        
+
 
         expect(
             screen.getByTestId("product-image")
@@ -47,21 +52,8 @@ describe("Product component", () => {
 
     });
 
-    it("adds a product to the cart", async()=>{
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
-
-        const loadCart = vi.fn();
-        render(<Product product={product} loadCart={loadCart}/>);
+    it("adds a product to the cart", async () => {
+        render(<Product product={product} loadCart={loadCart} />);
 
         const user = userEvent.setup();
         const addToCartButton = screen.getByTestId("add-to-cart-button");
@@ -75,9 +67,6 @@ describe("Product component", () => {
             }
         );
         expect(loadCart).toHaveBeenCalled();
-
     });
-
-
 
 });
